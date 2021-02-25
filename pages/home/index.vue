108c9a4b8e722d60f53f29c580fb0f10d9f589f1
wuwenjie1 <wuwenjie1@wingtech.com>
2021-02-25 15:15:07 +0800

    项目重构

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="">
     <h2>首页</h2>
-    <van-button type="default">默认按钮</van-button>
-    <!-- <van-search
-      value="value"
-      shape="round"
-      background="#4fc08d"
-      placeholder="请输入搜索关键词"
-    /> -->
+    1111111
   </div>
 </template>
 
@@ -15,7 +9,6 @@
 export default {
   data() {
     return {
-      value: ''
     };
   }
 }
--- unpackage/dist/dev/mp-weixin/pages/home/index.js
+++ unpackage/dist/dev/mp-weixin/pages/home/index.js
@@ -140,17 +140,10 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
-//
-//
-//
-//
-//
-//
 var _default =
 {
   data: function data() {
-    return {
-      value: '' };
+    return {};
 
   } };exports.default = _default;
 
