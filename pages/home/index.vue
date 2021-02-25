2c6e15e71c48de8c8b93f4014270d7f449decaae
wuwenjie1 <wuwenjie1@wingtech.com>
2021-02-25 17:30:18 +0800

    项目更新

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="">
-    <h2>首页</h2>
-    1111111
+    <uni-search-bar :radius="100" @confirm="search"></uni-search-bar>
+    <uni-swiper-dot :info="info" :current="current" field="content" :mode="mode">
+    <swiper class="swiper-box" @change="change">
+      <swiper-item v-for="(item ,index) in info" :key="index">
+        <view class="swiper-item">
+            {{item.content}}
+          </view>
+        </swiper-item>
+      </swiper>
+    </uni-swiper-dot>
   </div>
 </template>
 
@@ -9,8 +17,22 @@
 export default {
   data() {
     return {
+      info: [{
+                content: '内容 A'
+            }, {
+                content: '内容 B'
+            }, {
+                content: '内容 C'
+            }],
+            current: 0,
+            mode: 'round',
     };
-  }
+  },
+    methods: {
+        change(e) {
+            this.current = e.detail.current;
+        }
+    }
 }
 </script>
 <style scoped>
--- unpackage/dist/dev/mp-weixin/pages/home/index.js
+++ unpackage/dist/dev/mp-weixin/pages/home/index.js
@@ -98,6 +98,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "recyclableRender", function() { return recyclableRender; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "components", function() { return components; });
 var components
+try {
+  components = {
+    uniSearchBar: function() {
+      return __webpack_require__.e(/*! import() | components/uni-search-bar/uni-search-bar */ "components/uni-search-bar/uni-search-bar").then(__webpack_require__.bind(null, /*! @/components/uni-search-bar/uni-search-bar.vue */ 56))
+    },
+    uniSwiperDot: function() {
+      return __webpack_require__.e(/*! import() | components/uni-swiper-dot/uni-swiper-dot */ "components/uni-swiper-dot/uni-swiper-dot").then(__webpack_require__.bind(null, /*! @/components/uni-swiper-dot/uni-swiper-dot.vue */ 42))
+    }
+  }
+} catch (e) {
+  if (
+    e.message.indexOf("Cannot find module") !== -1 &&
+    e.message.indexOf(".vue") !== -1
+  ) {
+    console.error(e.message)
+    console.error("1. 排查组件名称拼写是否正确")
+    console.error(
+      "2. 排查组件是否符合 easycom 规范，文档：https://uniapp.dcloud.net.cn/collocation/pages?id=easycom"
+    )
+    console.error(
+      "3. 若组件不符合 easycom 规范，需手动引入，并在 components 中注册该组件"
+    )
+  } else {
+    throw e
+  }
+}
 var render = function() {
   var _vm = this
   var _h = _vm.$createElement
@@ -140,12 +166,33 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
 var _default =
 {
   data: function data() {
-    return {};
+    return {
+      info: [{
+        content: '内容 A' },
+      {
+        content: '内容 B' },
+      {
+        content: '内容 C' }],
 
-  } };exports.default = _default;
+      current: 0,
+      mode: 'round' };
+
+  },
+  methods: {
+    change: function change(e) {
+      this.current = e.detail.current;
+    } } };exports.default = _default;
 
 /***/ })
 ],[[11,"common/runtime","common/vendor"]]]);
